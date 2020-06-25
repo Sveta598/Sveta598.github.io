e8c4907edd9c878db0086378180e226cfcef63d2
Svetlana Pavlovskaya <sveta.pavl@tut.by>
2020-06-25 04:00:57 +0300

    project_final

--- js/navigation.js
+++ js/navigation.js
@@ -4,22 +4,10 @@ nav.addEventListener("mouseleave", hideNav, false);
 function showNav() {
     this.children[0].style.display = "block";
     this.children[0].style.background = "#A2DD21";
-    this.children[0].style.marginTop = "0";
+    this.children[0].style.marginTop = "5%";
     this.children[0].style.marginBottom = "0";
 }
 function hideNav() {
     this.children[0].style.display = "none";
 }
 
-var social = document.querySelector(".footer__icons");
-social.addEventListener("mouseenter", showIcons, false);
-social.addEventListener("mouseleave", hideIcons, false);
-function showIcons() {
-    this.children[0].style.display = "flex";
-    this.children[0].style.marginTop = "0";
-    this.children[0].style.marginBottom = "0";
-    this.children[0].style.marginLeft = "5%";
-}
-function hideIcons() {
-    this.children[0].style.display = "none";
-}
